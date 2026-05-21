7c9a837ab752ea61936e23a23e04e82f6d746302
Bramicuuu :) <158994218+bramicu13@users.noreply.github.com>
2026-05-21 08:58:35 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# forCup
+Esta es una herramienta para elegir el formato del videojuego "Cuphead" por si quieres jugar en una CRT o en su formato original.
+
+Proximamente se lanzara una version para windows
---
+++ forcup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+## CREADO POR BRAMICU
+
+43(){
+cp Assembly-CSharp-4-3.dll $ruta/Assembly-CSharp.dll && clear && echo 'Ahora puede ejecutar cuphead en 4:3'
+}
+
+nativo(){
+cp Assembly-CSharp-native.dll $ruta/Assembly-CSharp.dll && clear && echo 'Ahora puede ejecutar cuphead en 16:9'
+}
+
+opcion=""
+ruta=""
+clear
+echo '		-------forCup-------'
+echo '	En que formato queres ejecutar Cuphead?'
+echo ''
+echo '		1)4:3	     2)16:9'
+echo '			    (Nativo)'
+ruta=$(cat RUTA)
+read opcion
+
+if grep -x -q "porfavor-incluya-la-ruta-para-la-carpeta-de-cuphead" RUTA; then
+	clear
+	echo "Porfavor incluya una ruta exacta para modificar el formato de Cuphead."
+	exit 1
+fi
+
+case $opcion in
+1) 43
+;;
+2) nativo
+;;
+*) echo 'Porfavor ingrese una opcion valida'
+;;
+esac
+
